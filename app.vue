e9521ff64bffc76c853814dd6fddaa3ff6114a0d
Nawinkishore <nawinkishore2004@gmail.com>
2024-02-12 20:29:21 +0530

    Initial commit

---
+++ app.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <!-- Navbar -->
+    <section>
+      <div class="h-20 w-full flex  items-center justify-around bg-[#191919] text-white">
+        <div>LogoHere</div>
+        <div class=" flex items-center gap-28">
+          <ul class="flex items-center gap-4 text-nowrap">
+            <li>About</li>
+            <li>Collection</li>
+            <li>Creater</li>
+            <li>Road Map</li> 
+
+          </ul>
+          <button class="bg-[#da904f] h-10 w-full p-3 rounded-lg flex items-center justify-center border-2 border-white">Sign in</button>
+        </div>
+        
+      </div>
+    </section>
+    <!-- Navbar -->
+    <!-- 1st page -->
+    <section>
+      <div>
+        <div class="relative">
+          <img src="./assets/bg.svg" class="w-full" >
+          <div class="absolute top-0 left-0 p-20 flex items-center justify-center">
+            <div class="text-white">
+              <h1 class="text-8xl font-bold">We Have High Quality <span class="text-[#da904f]">NFT</span> Collections</h1>
+              <p class="mt-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere nihil minima libero earum laudantium tempora. Quos dolor asperiores consequatur non necessitatibus aspernatur. Aliquam ipsa accusamus reprehenderit nam veniam enim earum!</p>
+              <div class="flex items-center gap-10 mt-10">
+                <button class="h-10 w-[150px] p-3 flex items-center justify-center text-nowrap bg-[#da904f]  rounded-2xl">Get Whitelist</button>
+                <button class=" h-10 w-[150px] p-3 flex items-center justify-center text-nowrap bg-transparent border-2 border-white  rounded-2xl">Contact</button>
+
+              </div>
+            </div>
+            <div class="w-full">
+              <img src="./assets/tiger_hero.svg" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  </div>
+</template>

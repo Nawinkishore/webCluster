725dce57d92e05966c58d56b5f2a72030592752e
Nawinkishore <nawinkishore2004@gmail.com>
2024-02-12 21:26:01 +0530

    feat:aboutUs

--- app.vue
+++ app.vue
@@ -2,44 +2,125 @@
   <div>
     <!-- Navbar -->
     <section>
-      <div class="h-20 w-full flex  items-center justify-around bg-[#191919] text-white">
+      <div
+        class="h-20 w-full flex items-center justify-around bg-[#191919] text-white"
+      >
         <div>LogoHere</div>
-        <div class=" flex items-center gap-28">
+        <div class="flex items-center gap-28">
           <ul class="flex items-center gap-4 text-nowrap">
             <li>About</li>
             <li>Collection</li>
             <li>Creater</li>
-            <li>Road Map</li> 
-
+            <li>Road Map</li>
           </ul>
-          <button class="bg-[#da904f] h-10 w-full p-3 rounded-lg flex items-center justify-center border-2 border-white">Sign in</button>
+          <button
+            class="bg-[#da904f] h-10 w-full p-3 rounded-lg flex items-center justify-center border-2 border-white"
+          >
+            Sign in
+          </button>
         </div>
-        
       </div>
     </section>
     <!-- Navbar -->
-    <!-- 1st page -->
+    <!-- slide  1-->
     <section>
       <div>
         <div class="relative">
-          <img src="./assets/bg.svg" class="w-full" >
-          <div class="absolute top-0 left-0 p-20 flex items-center justify-center">
-            <div class="text-white">
-              <h1 class="text-8xl font-bold">We Have High Quality <span class="text-[#da904f]">NFT</span> Collections</h1>
-              <p class="mt-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere nihil minima libero earum laudantium tempora. Quos dolor asperiores consequatur non necessitatibus aspernatur. Aliquam ipsa accusamus reprehenderit nam veniam enim earum!</p>
-              <div class="flex items-center gap-10 mt-10">
-                <button class="h-10 w-[150px] p-3 flex items-center justify-center text-nowrap bg-[#da904f]  rounded-2xl">Get Whitelist</button>
-                <button class=" h-10 w-[150px] p-3 flex items-center justify-center text-nowrap bg-transparent border-2 border-white  rounded-2xl">Contact</button>
-
+          <img src="./assets/bg.svg" class="w-full " />
+          <div class="absolute top-0 left-0 p-20">
+            <div class="flex justify-center items-center">
+              <div class="text-white">
+                <h1 class="text-8xl font-bold">
+                  We Have High Quality
+                  <span class="text-[#da904f]">NFT</span> Collections
+                </h1>
+                <p class="mt-5">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Facere nihil minima libero earum laudantium tempora. Quos
+                  dolor asperiores consequatur non necessitatibus aspernatur.
+                  Aliquam ipsa accusamus reprehenderit nam veniam enim earum!
+                </p>
+                <div class="flex items-center gap-10 mt-10">
+                  <button
+                    class="h-10 w-[150px] p-3 flex items-center justify-center text-nowrap bg-[#da904f] rounded-2xl"
+                  >
+                    Get Whitelist
+                  </button>
+                  <button
+                    class="h-10 w-[150px] p-3 flex items-center justify-center text-nowrap bg-transparent border-2 border-white rounded-2xl"
+                  >
+                    Contact
+                  </button>
+                </div>
+              </div>
+              <div class="w-full">
+                <img src="./assets/tiger_hero.svg" alt="" />
               </div>
             </div>
-            <div class="w-full">
-              <img src="./assets/tiger_hero.svg" alt="">
+            <div
+              class="text-white flex w-[50%] justify-between text-2xl bg-white/10 p-5 rounded-xl"
+            >
+              <div>
+                <h1>240k+</h1>
+                <p>Total sale</p>
+              </div>
+              <div>
+                <h1>100k+</h1>
+                <p>Auctions</p>
+              </div>
+              <div>
+                <h1>240k+</h1>
+                <p>Artists</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </section>
+    <!-- About us slide -->
+    <div class="bg-black h-40 w-full -mt-1 text-white flex items-center justify-center text-2xl">
+      <h1>About us</h1>
+    </div>
 
+    <!-- slide 2 -->
+    <section>
+      <div class="bg-[#150e08] flex items-start justify-between gap-10 px-28 py-3">
+          <div class="grid grid-cols-2 gap-10 w-[50%]">
+            <img src="./assets/image 104.png" alt="" class="h-[200px] w-[400px] bg-[#232220] p-3 rounded-lg ">
+            <img src="./assets/image 105.png" alt="" class="h-[200px] w-[400px] bg-[#232220] p-3 rounded-lg">
+            <img src="./assets/image 106.png" alt="" class="h-[200px] w-[400px] bg-[#232220] p-3 rounded-lg">
+            <img src="./assets/image 107.png" alt="" class="h-[200px] w-[400px] bg-[#232220] p-3 rounded-lg">
+
+          </div>
+          <div class="text-white w-[50%]">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus omnis placeat, natus et quisquam iusto asperiores magni tempora alias officia? Obcaecati officiis maxime delectus sint deleniti beatae dicta et facere.
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi minus error maiores iure voluptas molestias aperiam beatae aut, quos fugiat exercitationem, tempore ad? Mollitia esse in ex debitis cupiditate quibusdam.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet tempora quisquam magni est, velit nostrum sint odit. Dicta autem corrupti veritatis, assumenda architecto rem, vero recusandae est sit provident eum.
+              
+            </p>
+            <p class="mt-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta illo ipsam omnis veritatis autem repellendus placeat consequuntur nisi minima ut.
+            </p>
+            <button class="mt-10 bg-white/20 h-fit w-fit p-3 rounded-xl border-orange-400 border-2">Connect Wallet</button>
+          </div>
+      </div>
+    </section>
+    <!-- top collections -->
+    <div class="bg-black h-48 w-full text-white flex items-center justify-center ">
+     <div>
+      <h1 class="flex items-center justify-center text-5xl font-bold">Top Collections</h1>
+      <p class="leading-normal mt-5">The largest and unique Super rare NFT marketplace
+        <span class="flex items-center justify-center">For crypto-collectibles span</span></p>
+     </div>
+    </div>
+    <!-- slide 3 -->
+    <section>
+      <div>
+        <img src="./assets/bg.svg" alt="">
+        <div>
+
+        </div>
+      </div>
+    </section>
   </div>
 </template>
